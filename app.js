8b13090748e420996f70749b239d046467667d6e
somang <somang.nam@gmail.com>
2021-10-21 09:59:59 -0400

    update:wq

---
+++ app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var router = express.Router();
+
+
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname+'/index.html'));
+});
+
+
+// add the router
+app.use('/', router);
+app.use(express.static(__dirname + '/public'));
+
+app.listen(process.env.port || 3000, function () {
+	console.log('listening at port');
+});
